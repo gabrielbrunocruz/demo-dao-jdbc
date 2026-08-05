645ef140708e1c713453487c0b036129fbbbdee8
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-08-04 22:56:11 -0300

    SellerDaoJDBC and DaoFactory

--- src/model/dao/DepartmentDao.java
+++ src/model/dao/DepartmentDao.java
@@ -1,5 +1,15 @@
 package model.dao;
 
+import java.util.List;
+
+import model.entities.Department;
+
 public interface DepartmentDao {
+	
+	void insert(Department obj);
+	void Update(Department obj);
+	void deleteById(Integer id);
+	Department findByid(Integer id);
+	List<Department> findAll();
 
 }
