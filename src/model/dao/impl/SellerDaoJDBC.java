68b2ce5224bdffefef11b063bc1abfabd174a5aa
Gabriel Bruno da Cruz <gb3268147@gmail.com>
2026-08-05 21:09:09 -0300

    findByDepartment implementation

--- src/model/dao/impl/SellerDaoJDBC.java
+++ src/model/dao/impl/SellerDaoJDBC.java
@@ -5,9 +5,10 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
-
-import com.mysql.cj.protocol.Resultset;
+import java.util.Map;
 
 import db.DB;
 import db.DbException;
@@ -96,6 +97,52 @@ public class SellerDaoJDBC implements SellerDao {
 	@Override
 	public List<Seller> findAll() {
 		return null;
+	}
+
+	@Override
+	public List<Seller> findByDepartment(Department department) {
+		PreparedStatement st = null;
+		ResultSet rs = null;
+		
+		try {
+		st=conn.prepareStatement(
+				"SELECT seller. *, department.Name as DepName "
+				+ "FROM seller INNER JOIN department "
+				+ "ON seller.DepartmentId = department.Id "
+				+ "WHERE DepartmentId = ? "
+				+ "ORDER BY Name");
+		
+				
+				
+		
+		st.setInt(1, department.getId());
+		rs = st.executeQuery();
+		List<Seller>sellers = new ArrayList<>();
+        Map<Integer, Department> map = new HashMap<>();
+		while(rs.next()) {
+			
+			Department dep = map.get(rs.getInt("DepartmentId"));
+			//se nao houver uma chave com esse valor dentro do map vai retornar null;
+			//fazemos isso para nao retornar um departamento repetido, tendo um mesmo departamento com os vendedores
+			
+			if(dep ==null) {
+				dep = instantiateDepartment(rs);
+				map.put(rs.getInt("DepartmentId"), dep);
+			}
+			Seller obj = instanciateSeller(rs, dep);
+			sellers.add(obj);
+		}
+		return sellers;
+				
+		}
+		catch(SQLException e ) {
+			throw new DbException(e.getMessage());
+		}
+		finally {
+			DB.closeStatement(st);
+			DB.closeResultSet(rs);
+		}
+		
 	}
 
 }
